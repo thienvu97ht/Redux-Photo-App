616acae57809ac808fe496498bd3fe1aaef97ba0
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-06-28 15:55:25 +0700

    Add Router

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+NotFound.propTypes = {};
+
+function NotFound(props) {
+  return <div>Ooop ... Not Found</div>;
+}
+
+export default NotFound;
---
+++ src/features/Photo/pages/Main/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+MainPage.propTypes = {};
+
+function MainPage(props) {
+  return <div>MainPage</div>;
+}
+
+export default MainPage;
